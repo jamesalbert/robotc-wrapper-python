88292414e2b290863200aabc4338c21676eeb8b0
jamesalbert <jbert@salsa.alphalyrae.net>
2012-10-03 00:42:14 -0700

    pypi release .5

--- lib/robotc/utils.py
+++ lib/robotc/utils.py
@@ -6,49 +6,41 @@ robot = __init__.robotcwrapper()
 
 class robotcwrapperutils:
 
-    def SETUP(self):
-        robot.pragma(2, "button", "Touch")
+    def SETUP(self, sensor_in, sensor_name, sensor_type, reflect_port, reflect_switch, auto_switch):
+        robot.pragma(sensor_in, sensor_name, sensor_type)
 
-        robot.reflect(2, "true")
-        robot.auto("false")
+        robot.reflect(reflect_port, reflect_switch)
+        robot.auto(auto_switch)
 
-    def FORWARD(self):
+    def FORWARD(self, port_1, port_2, speed):
         robot.start_void("forward")
-        robot.motor(2, 127)
-        robot.motor(1, 127)
+        robot.motor(port_1, speed)
+        robot.motor(port_2, speed)
         robot.end()
 
-    def REVERSE(self):
+    def REVERSE(self, port_1, port_2, speed):
         robot.start_void("reverse")
-        robot.motor(2, -127)
-        robot.motor(1, -127)
+        robot.motor(port_1, -(speed))
+        robot.motor(port_2, -(speed))
         robot.end()
 
-    def TURN_LEFT(self):
+    def TURN_LEFT(self, port_1, port_2, speed, duration):
         robot.start_void("turn_left")
-        robot.motor(2, 127)
-        robot.motor(1, -127)
-        robot.wait(1)
+        robot.motor(port_1, -(speed))
+        robot.motor(port_2, speed)
+        robot.wait(duration)
         robot.end()
 
-    def TURN_RIGHT(self):
+    def TURN_RIGHT(self, port_1, port_2, speed, duration):
         robot.start_void("turn_right")
-        robot.motor(2, -127)
-        robot.motor(1, 127)
-        robot.wait(1)
+        robot.motor(port_1, -(speed))
+        robot.motor(port_2, speed)
+        robot.wait(duration)
         robot.end()
 
-    def SUSPEND(self):
+    def SUSPEND(self, port_1, port_2):
         robot.start_void("stop")
-        robot.motor(2, 0)
-        robot.motor(1, 0)
+        robot.motor(port_1, 0)
+        robot.motor(port_2, 0)
         robot.end()
-
-    def BASIC_MOVEMENTS(self):
-        self.SETUP()
-        self.FORWARD()
-        self.REVERSE()
-        self.TURN_LEFT()
-        self.TURN_RIGHT()
-        self.SUSPEND()
 
--- lib/setup.py
+++ lib/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name = "robotc-wrapper",
     packages = ["robotc"],
-    version = "0.4",
+    version = "0.5",
     description = "A Python 2.7 wrapper for RobotC and Vex PIC robot",
     author = "James Albert",
     author_email = "james.albert72@gmail.com",
